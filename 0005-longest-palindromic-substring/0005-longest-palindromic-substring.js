cf17d89bdcacfeb861d3a3c4a1ba0592903a07e7
양수보 <156686089+Farmer15@users.noreply.github.com>
2024-06-14 22:50:01 +0900

    Time: 404 ms (23.78%), Space: 55.8 MB (31.49%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  let start = 0;
+  let end = s.length;
+  let count = s.length - 1;
+
+  while(start < s.length) {
+    if (isPalindrome(s.slice(start, end))) {
+      return s.slice(start, end);
+    }
+
+    if (end === s.length) {
+      start = 0;
+      end = start + count;
+      count--;
+      continue;
+    }
+
+    start++;
+    end++;
+  }
+};
+
+function isPalindrome (str) {
+  for (let i = 0; i <= str.length / 2 - 1; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
