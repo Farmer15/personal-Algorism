5725d3082ce4eaf28407b935d25b57d7bc6015fa
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2024-11-02 13:53:22 +0900

    Time: 159 ms (6.6%), Space: 26 MB (92.1%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,25 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        left_pointer = 0
-        right_pointer = 0
-        best_buy_and_sell = 0
+        minPrice = prices[0]
+        maxProfit = 0
+        pointer = 0
 
-        while left_pointer < len(prices) -1 or right_pointer < len(prices) - 1:
-            right_number = prices[right_pointer]
-            left_number = prices[left_pointer]
-            best_buy_and_sell = max(best_buy_and_sell, right_number - left_number)
-            
-            if left_pointer == right_pointer:
-                right_pointer += 1
-                continue
+        while pointer < len(prices):
+            current_price = prices[pointer]
+            maxProfit = max(current_price - minPrice, maxProfit)
+            minPrice = min(current_price, minPrice)
+            pointer += 1
 
-            if right_pointer == len(prices) - 1:
-                left_pointer += 1
-                continue
-
-            if right_number - left_number >= 0:
-                right_pointer += 1
-            else:
-                left_pointer = right_pointer
-
-        return best_buy_and_sell
+        return maxProfit
