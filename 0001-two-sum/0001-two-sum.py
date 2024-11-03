7b9ea80cc2ffd1b909f4bf0deb67089db2dd66f3
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2024-11-03 19:49:20 +0900

    Time: 3 ms (54.2%), Space: 18.9 MB (6.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,18 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sorted_nums = sorted(nums[:])
-        print(sorted_nums)
-        left_pointer = 0
-        right_pointer = len(sorted_nums) - 1
+        chached = {}
 
-        while left_pointer <= right_pointer:
-            left_number = sorted_nums[left_pointer]
-            right_number = sorted_nums[right_pointer]
-            
-            if left_number + right_number == target:
-                return [nums.index(left_number), len(nums) - 1 - nums[::-1].index(right_number)]
-
-            if left_number + right_number > target:
-                right_pointer -= 1
+        for nums_index in range(len(nums)):
+            if nums[nums_index] in chached:
+                return [chached[nums[nums_index]][1], nums_index]
             else:
-                left_pointer += 1
+                chached[target - nums[nums_index]] = [nums[nums_index], nums_index]
