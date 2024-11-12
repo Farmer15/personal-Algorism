5766c315906b7b008d8410e285e67c9e77b7f32e
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2024-11-12 11:11:33 +0900

    Time: 0 ms (100%), Space: 16.7 MB (67.61%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,15 +1,18 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int, initialNum: int = None) -> List[List[int]]:
-        if initialNum is None: 
-            initialNum = image[sr][sc]
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        initialNum = image[sr][sc]
+
+        return self.changeColor(image, sr, sc, color, initialNum)
+
+    def changeColor(self, image: List[List[int]], sr: int, sc: int, color: int, initialNum: int) -> List[List[int]]:
         if sr < 0 or sc < 0 or sr >= len(image) or sc >= len(image[0]) or image[sr][sc] != initialNum or image[sr][sc] == color :
             return image
-        initial_color = image[sr][sc]
-        image[sr][sc] = color
 
-        self.floodFill(image, sr - 1, sc, color, initial_color)
-        self.floodFill(image, sr + 1, sc, color, initial_color)
-        self.floodFill(image, sr, sc - 1, color, initial_color)
-        self.floodFill(image, sr, sc + 1, color, initial_color)
+        initial_color, image[sr][sc] = image[sr][sc], color
+
+        self.changeColor(image, sr - 1, sc, color, initial_color)
+        self.changeColor(image, sr + 1, sc, color, initial_color)
+        self.changeColor(image, sr, sc - 1, color, initial_color)
+        self.changeColor(image, sr, sc + 1, color, initial_color)
 
         return image
