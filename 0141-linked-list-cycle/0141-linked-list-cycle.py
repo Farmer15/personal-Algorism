180afe096158a04583b0600059377cbccd700e66
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2024-11-20 15:53:39 +0900

    Time: 55 ms (7.34%), Space: 18.9 MB (95.58%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,18 +6,17 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        try:
-          one_jump_node = head.next
-          two_jump_node = head
+        if not head or not head.next:
+            return False
 
-          while two_jump_node:
-              print(two_jump_node.val, one_jump_node.val)
-              if two_jump_node == one_jump_node:
+        one_jump_node = head
+        two_jump_node = head
+
+        while two_jump_node and two_jump_node.next:
+            one_jump_node = one_jump_node.next
+            two_jump_node = two_jump_node.next.next
+
+            if one_jump_node == two_jump_node:
                 return True
-              
-              one_jump_node = one_jump_node.next
-              two_jump_node = two_jump_node.next.next
 
-          return False
-        except:
-          return False
+        return False
