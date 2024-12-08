5caab27459e8462ccb3642c1fa727acd254edc65
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2024-12-08 15:32:37 +0900

    Time: 53 ms (55.9%), Space: 59.8 MB (59.24%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.js
+++ 0542-01-matrix/0542-01-matrix.js
@@ -5,39 +5,31 @@
 var updateMatrix = function(mat) {
   const chachQueue = [];
   const DIRECTIONS = [[0, 1], [0, -1], [-1, 0], [1, 0]];
-  const resultMat = Array.from(Array(mat.length), (row) => Array(mat[0].length).fill(null));
+  const TOTAL_COUNT = mat.length * mat[0].length;
 
   for (let row = 0; row < mat.length; row++) {
     for (let column = 0; column < mat[0].length; column++) {
       if (mat[row][column] === 0) {
         chachQueue.push([row, column]);
+      } else {
+        mat[row][column] = TOTAL_COUNT
       }
     }
   }
 
-  let count = 0
-
   while (chachQueue.length !== 0) {
-    console.log("시작", chachQueue)
-    const queueLength = chachQueue.length;
+    [currentposX, currentposY] = chachQueue.shift();
 
-    for (let i = 0; i < queueLength; i++) {
-      const [posX, posY] = chachQueue.shift();
+    DIRECTIONS.forEach(([directionPosX, directionPosY]) => {
+      const [nextPosX, nextPosY] = [currentposX +  directionPosX, currentposY + directionPosY];
+      const nextPos = mat[nextPosX]?.[nextPosY];
 
-      resultMat[posX][posY] = count;
-
-      DIRECTIONS.forEach(([directionPosX, directionPoxY]) => {
-        const [nextPosX, nextPosY] = [directionPosX + posX, directionPoxY + posY]
-        
-        if (resultMat[nextPosX]?.[nextPosY] === null && mat[nextPosX][nextPosY]) {
-
-          chachQueue.push([nextPosX, nextPosY])
-        }
-      })
-    }
-    console.log("끝", chachQueue)
-    count++;
+      if (nextPos && nextPos === TOTAL_COUNT) {
+        mat[nextPosX][nextPosY] = mat[currentposX][currentposY] + 1;
+        chachQueue.push([nextPosX, nextPosY]);
+      }
+    })
   }
 
-  return resultMat
+  return mat
 };
