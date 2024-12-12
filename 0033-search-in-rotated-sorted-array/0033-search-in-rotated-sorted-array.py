55de5b2df00e109111765b2d8b881b06b951b85b
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2024-12-12 11:48:52 +0900

    Time: 0 ms (100%), Space: 17.7 MB (9.1%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,23 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int, start: int = 0, end = None) -> int:
-        if end is None:
-            end = len(nums) - 1
+        start = 0
+        end = len(nums) - 1
 
-        if start == end:
-            if nums[start] == target:
-                return start
-            else:
-                return -1
+        while start != end:
+            middle_index = (end + start) // 2 
 
-        middle_index = (end + start) // 2 
+            if nums[start] <= nums[middle_index]:
+                if nums[start] <= target <= nums[middle_index]:
+                    end = middle_index
+                else:
+                    start = middle_index + 1
+            else:
+                if nums[middle_index] < target <= nums[end]:
+                    start = middle_index + 1
+                else:
+                    end = middle_index
 
-        if nums[start] <= nums[middle_index]:
-            if nums[start] <= target <= nums[middle_index]:
-                return self.search(nums, target, start, middle_index)
-            else:
-                return self.search(nums, target, middle_index + 1, end)
-        else:
-            if nums[middle_index] < target <= nums[end]:
-                return self.search(nums, target, middle_index + 1, end)
-            else:
-                return self.search(nums, target, start, middle_index)
+
+        return start if nums[start] == target else -1
