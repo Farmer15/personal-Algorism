b30b6bbcfa3ab6a5d4622899b5f8ca0e6c0305e4
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2024-12-13 16:48:02 +0900

    Time: 15 ms (32.71%), Space: 17.2 MB (23.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -6,17 +6,18 @@ class Solution:
 
         def get_combination_sum(remaining: int, combination: List[int], start: int):
             if remaining == 0:
-                result_list.append(list(combination))  # 깊은 복사로 결과 저장
-                return
-
+                result_list.append(combination)
+            
             if remaining < 0:
                 return
             
-            for i in range(start, len(candidates)):
-                combination.append(candidates[i])
-                get_combination_sum(remaining - candidates[i], combination, i) 
-                combination.pop()
+            for index in range(start, len(candidates)):
+                candidate = candidates[index]
+                get_combination_sum(remaining - candidate, combination + [candidate], index)
+
+            return
 
         get_combination_sum(target, [], 0)
+
         return result_list
         
