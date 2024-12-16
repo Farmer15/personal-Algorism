51e87eeb82e6231a908b24e11872c1d7c448068b
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2024-12-16 22:33:07 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hash_map = {}
+
+        for num in nums:
+            if num in hash_map: 
+                hash_map[num] += 1
+
+                if hash_map[num] >= len(nums) / 2:
+                    return num
+            else:
+                hash_map[num] = 1
