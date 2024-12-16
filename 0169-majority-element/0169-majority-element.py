8608d62596aaa18f378b08e68146fe7e2ba8804d
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2024-12-16 22:33:44 +0900

    Time: 19 ms (7.61%), Space: 18.9 MB (8.25%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -5,8 +5,8 @@ class Solution:
         for num in nums:
             if num in hash_map: 
                 hash_map[num] += 1
-
-                if hash_map[num] >= len(nums) / 2:
-                    return num
             else:
                 hash_map[num] = 1
+
+            if hash_map[num] >= len(nums) / 2:
+                return num
