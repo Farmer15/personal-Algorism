e5d97acce23041c86cded49a1ca561e6761bdfd0
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-01-03 16:37:14 +0900

    Time: 0 ms (100%), Space: 18 MB (8.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,34 +1,21 @@
 class Solution:
     def sortColors(self, nums: List[int], start: int = 0, end: int = None) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        if end is None:
-            end = len(nums) - 1
+        left = 0
+        mid = 0
+        right = len(nums) - 1
 
-        if end - start <= 1:
-            if nums[start] > nums[end]:
-                [nums[start], nums[end]] = [nums[end], nums[start]]
-            return
-        
-        fivot = start
-        left_pointer = start + 1
-        right_pointer = end
-
-        while left_pointer < right_pointer:
-            if nums[left_pointer] > nums[right_pointer]:
-                [nums[left_pointer], nums[right_pointer]] = [nums[right_pointer], nums[left_pointer]]
+        while mid <= right:
+            if nums[mid] == 0:
+                [nums[left], nums[mid]] = [nums[mid], nums[left]]
+                left += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
             else:
-                if nums[left_pointer] < nums[fivot]:
-                    left_pointer += 1
-                
-                if nums[right_pointer] >= nums[fivot]:
-                    right_pointer -= 1
-        
-        [nums[fivot], nums[right_pointer]] = [nums[right_pointer] ,nums[fivot]]
-        self.sortColors(nums, start, right_pointer)
-        self.sortColors(nums, right_pointer, end)
+                [nums[mid], nums[right]] = [nums[right], nums[mid]]
+                right -= 1
 
+ 
 
 
 
