df8e07c94ffbf2937da6cfeaccbee87ac55ee254
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-01-20 11:59:22 +0900

    Time: 0 ms (100%), Space: 18 MB (16.23%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,6 +1,6 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        result = ""
+        result = 0
         is_positive = True
         s = s.strip()
 
@@ -14,18 +14,14 @@ class Solution:
                 continue
             
             if char.isdigit():
-                result += char
+                result = 10 * result + int(char)
+
+                if result > 2 ** 31 - 1:
+                    result = 2 ** 31 - 1 if is_positive else 2 ** 31
+                    break
             else:
                 break
-
-        if not result:
-            return 0
-
-        result = int(result)
-
-        if result > 2 ** 31 - 1:
-            result = 2 ** 31 - 1 if is_positive else 2 ** 31
-        
+      
         return result if is_positive else -1 * result
         
             
