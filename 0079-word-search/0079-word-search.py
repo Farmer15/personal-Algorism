6dc9ec33b5bd5ad0be64b476ee7fe90a1a569e68
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-02-02 10:23:04 +0900

    Time: 7089 ms (8.54%), Space: 17.7 MB (70.15%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -6,30 +6,31 @@ class Solution:
         for row in range(len(board)):
             for column in range(len(board[row])):
                 if board[row][column] == word[0]:
-                    if self.search_word(board, word, "", set(), (row, column)):
+                    if self.search_word(board, word, "", (row, column)):
                         return True
         
         return False
     
-    def search_word(self, board: List[List[str]], word: str, confirm_word: str, visited: set, position: tuple) -> bool:
+    def search_word(self, board: List[List[str]], word: str, confirm_word: str, position: tuple) -> bool:
         x, y = position[0], position[1]
         current_word = board[x][y]
         
-        if (x, y) in visited or len(confirm_word) >= len(word) or word[len(confirm_word)] != current_word:
+        if len(confirm_word) >= len(word) or word[len(confirm_word)] != current_word:
             return False
         
         if confirm_word + current_word == word:
             return True 
 
-        visited.add((x, y))
-
         for next_dir_x, next_dir_y in self.DIRECTIONS:
             if not 0 <= x + next_dir_x <= len(board) - 1 or not 0 <= y + next_dir_y <= len(board[0]) - 1:
                 continue
+            
+            temp, board[x][y] = board[x][y], "#"
 
-            if self.search_word(board, word, confirm_word + current_word, { *visited }, (x + next_dir_x, y + next_dir_y)):
+            if self.search_word(board, word, confirm_word + current_word, (x + next_dir_x, y + next_dir_y)):
                 return True
             
+            board[x][y] = temp
     
         return False
         
