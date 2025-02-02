a71d33916979e781e3d0218997d43f769226299a
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-02-02 10:16:41 +0900

    Time: 9861 ms (5.01%), Space: 17.9 MB (32.1%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,35 @@
+class Solution:
+    def __init__(self):
+        self.DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        for row in range(len(board)):
+            for column in range(len(board[row])):
+                if board[row][column] == word[0]:
+                    if self.search_word(board, word, "", set(), (row, column)):
+                        return True
+        
+        return False
+    
+    def search_word(self, board: List[List[str]], word: str, confirm_word: str, visited: set, position: tuple) -> bool:
+        x, y = position[0], position[1]
+        current_word = board[x][y]
+        
+        if (x, y) in visited or len(confirm_word) >= len(word) or word[len(confirm_word)] != current_word:
+            return False
+        
+        if confirm_word + current_word == word:
+            return True 
+
+        visited.add((x, y))
+
+        for next_dir_x, next_dir_y in self.DIRECTIONS:
+            if not 0 <= x + next_dir_x <= len(board) - 1 or not 0 <= y + next_dir_y <= len(board[0]) - 1:
+                continue
+
+            if self.search_word(board, word, confirm_word + current_word, { *visited }, (x + next_dir_x, y + next_dir_y)):
+                return True
+            
+    
+        return False
+        
