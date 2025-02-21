2c5fdcce640b8c109fab8bca50c35e7c6663ff6d
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-02-21 11:22:07 +0900

    Time: 0 ms (100%), Space: 18 MB (14.69%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,7 +3,7 @@ class Solution:
         return self.switchNextPosition(nums)
     
     def switchNextPosition(self, nums: List[int]) -> None:
-        for posA in range(len(nums) - 1, -1, -1):
+        for posA in range(len(nums) - 2, -1, -1):
             for posB in range(len(nums) - 1, posA, -1):
                 if nums[posB] > nums[posA]:
                     [nums[posB], nums[posA]] = [nums[posA], nums[posB]]
