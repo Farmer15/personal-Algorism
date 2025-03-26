afabfdc49edf812fe7da4ec9f2b46dcb97e0de3a
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-03-26 12:45:59 +0900

    Time: 0 ms (100%), Space: 19.1 MB (47.82%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,53 +1,15 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        def search_near_index(arr: List[int], num: int) -> int:
-            start, end = 0, len(arr) - 1
-            result = -1
+        low = 0
+        high = len(arr) - k
 
-            while start <= end:
-                mid = (start + end) // 2
-                if arr[mid] <= num:
-                    result = mid
-                    start = mid + 1
-                else:
-                    end = mid - 1
+        while low < high:
+            start_point = (low + high) // 2
 
-            if start <= 0:
-                return 0
-            
-            if start > len(arr) - 1:
-                return len(arr) - 1
-            
-            if arr[start - 1] - num < num - arr[start]:
-                return start
+            if x - arr[start_point] <= arr[start_point + k] - x:
+                high = start_point
             else:
-                return start - 1
+                low = start_point + 1
+        
+        return arr[low: low + k] 
 
-        middle = search_near_index(arr, x)
-        left = middle - 1
-        right = middle + 1
-        result = deque([arr[middle]])
-
-        while k > 1:
-            if left < 0:
-                result.append(arr[right])
-                right += 1
-                k -= 1
-                continue
-            
-            if right > len(arr) - 1:
-                result.appendleft(arr[left])
-                left -= 1
-                k -= 1
-                continue
-
-            if x - arr[left] <= arr[right] - x:
-                result.appendleft(arr[left])
-                left -= 1
-            else:
-                result.append(arr[right])
-                right += 1
-            
-            k -= 1
-
-        return list(result)
