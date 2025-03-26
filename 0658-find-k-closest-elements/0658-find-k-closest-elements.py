26a5b8ac8667700118d09f2b2505cfa03106dbbe
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-03-26 12:08:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -0,0 +1,53 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        def search_near_index(arr: List[int], num: int) -> int:
+            start, end = 0, len(arr) - 1
+            result = -1
+
+            while start <= end:
+                mid = (start + end) // 2
+                if arr[mid] <= num:
+                    result = mid
+                    start = mid + 1
+                else:
+                    end = mid - 1
+
+            if start <= 0:
+                return 0
+            
+            if arr[start - 1] - num < num - arr[start]:
+                return start
+            else:
+                return start - 1
+
+        if len(arr) == 1:
+            return arr
+
+        middle = search_near_index(arr, x)
+        left = middle - 1
+        right = middle + 1
+        result = deque([arr[middle]])
+
+        while k > 1:
+            if left < 0:
+                result.append(arr[right])
+                right += 1
+                k -= 1
+                continue
+            
+            if right > len(arr) - 1:
+                result.appendleft(arr[left])
+                left -= 1
+                k -= 1
+                continue
+
+            if x - arr[left] <= arr[right] - x:
+                result.appendleft(arr[left])
+                left -= 1
+            else:
+                result.append(arr[right])
+                right += 1
+            
+            k -= 1
+
+        return list(result)
